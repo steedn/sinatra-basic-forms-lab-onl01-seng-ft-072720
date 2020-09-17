b68b3d7e4ba4fd37a0459d10b715a6fb4f5fa642
Nathan Steed <steedn@outlook.com>
2020-09-17 04:28:15 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,9 +1,14 @@
 class Puppy
   attr_accessor :name, :breed, :age
+  @@all = []
   def initialize(name, breed, age)
     @name, @breed, @age = name, breed, age
   end
 
   def save
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
